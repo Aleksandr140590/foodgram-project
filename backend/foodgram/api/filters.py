ee9dd5ac2a0fcf4f59a9286c0851c1cbfbd6d37f
Aleksandr140590 <alex2717@yandex.kz>
2023-04-05 22:12:14 +0600

    Update filters.py

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -6,8 +6,7 @@ class RecipeFilter(filters.FilterSet):
     is_favorited = filters.Filter(field_name='is_favorited')
     is_in_shopping_cart = filters.Filter(field_name='is_in_shopping_cart')
     author = filters.Filter(field_name='author__id')
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug',
-                                           lookup_expr='contains')
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
 
     class Meta:
         model = Recipe
